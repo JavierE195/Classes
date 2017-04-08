b0cc18c0c2a0103cc1ffccc899048c9d28d28b7a
JavierE195 <ellerbrockjavier@hotmail.com>
2017-04-08 13:46:28 -0500

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication34
+{
+    class Employee : Person
+    {
+        public double salary;
+
+        public Employee()
+        {
+            salary = 5000000;
+        }
+
+        public Employee(double _salary)
+        {
+            salary = _salary;
+        }
+
+        public void PrintEmployee()
+        {
+            Console.WriteLine("Salary: " + salary);
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication34
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public static int count = 0;
+
+        public Person()
+        {
+            firstName = "Henry";
+            lastName = "Ford";
+            count++;
+        }
+
+        public Person(string _firstName, string _lastName)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            count++;
+        }
+
+        public void PrintPerson()
+        {
+            Console.WriteLine("\nName of employee number " + count + " : "  + firstName + " " + lastName);
+        }
+    }
+}
